cb811c3fc74b72aca62cfa5f13eb27f0355775cf
AhmadSaleh99 <saleehsale816@gmail.com>
2025-07-13 16:53:54 +0300

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="links flex space-x-7 ">
+    <footer className="flex flex-col items-center my-4  border-t border-gray-300">
+      <div className="links flex my-4 justify-between w-3/5">
         <Link href="/" className="hover:text-primary transition ease-in">
           Home
         </Link>
@@ -28,8 +28,8 @@ const Footer = () => {
           </button> */}
       </div>
 
-      <div>
-        <div className="socials">
+      <div className="flex flex-col items-center gap-4 my-4">
+        <div className="socials flex gap-4">
           <img src="/images/instagram.png" alt="Instagram" />
           <img src="/images/facebook.png" alt="Facebook" />
           <img src="/images/twiter.png" alt="Twiter" />
